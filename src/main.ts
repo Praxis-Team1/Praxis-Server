a878d77eca0dacf9e83d2408c346a713d8d7a3b4
Jerson Mendoza <jersondavidma@gmail.com>
2018-09-05 02:36:42 -0500

    change main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join }  from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const SERVER_PORT = process.env.PORT || 3000;
+  const CLIENT_FILES = join(__dirname, '..', '..', 'client', 'dist');
+
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
