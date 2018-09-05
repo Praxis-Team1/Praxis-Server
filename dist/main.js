7edb4741447d4e67e32871865e69c565d73703fb
Jerson Mendoza <jersondavidma@gmail.com>
2018-09-05 13:08:26 -0500

    Update buildspec.yml

---
+++ dist/main.js
@@ -0,0 +1,23 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const path_1 = require("path");
+function bootstrap() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const app = yield core_1.NestFactory.create(app_module_1.AppModule);
+        const SERVER_PORT = process.env.PORT || 3000;
+        const CLIENT_FILES = path_1.join(__dirname, '..', '..', 'client', 'dist');
+        yield app.listen(SERVER_PORT);
+    });
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
