dac1ad35f5127f2958924fdc93bd7b5de9d187e8
sebastianpinosanchez <spinos@unal.edu.co>
2018-09-26 02:10:47 -0500

    Defined CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const SERVER_PORT = process.env.PORT || 3000;
   const CLIENT_FILES = join(__dirname, '..', '..', 'client', 'dist');
 
