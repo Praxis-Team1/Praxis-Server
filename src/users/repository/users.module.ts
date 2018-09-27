13428055d00f85179486616d3af3c7aa3e9e6b22
sebastianpinosanchez <spinos@unal.edu.co>
2018-09-26 20:26:54 -0500

    Fixed imports in .ts

--- src/users/repository/users.module.ts
+++ src/users/repository/users.module.ts
@@ -5,11 +5,11 @@ import { UsersService } from './users.service'
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../dto/users.entity';
-import { AdministratorsModule } from 'administrators/administrators.module';
-import { StudentsModule } from 'students/repository/students.module';
+import { AdministratorsModule } from '../../administrators/administrators.module';
+import { StudentsModule } from '../../students/repository/students.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AdministratorsModule, forwardRef(() =>  StudentsModule)],
+  imports: [TypeOrmModule.forFeature([User]), AdministratorsModule, forwardRef(() => StudentsModule)],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService]
